d0f5529fa1f0559e0faddf1d3ade57f02a032b1d
yarenalbayr <61636538+yarenalbayr@users.noreply.github.com>
2020-09-09 19:29:20 +0300

    Add files via upload
    In a list of month's names in, computer asks to user a number between 1-12 and says which month's name that number refers to.

---
+++ numOfMonth.py
@@ -0,0 +1,5 @@
+
+months = ['January' , 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+i = int(input("Give me a number between 1-12 and i give you which month it refers. "))
+nameOfMonth = months[i-1]
+print(nameOfMonth)
